049b27c5d12dedf75a1b21a2bd88bf2cd53e3ec7
Jason Dugger <jason_r_dugger@homedepot.com>
2016-06-17 18:57:32 -0500

    initial

---
+++ area.py
@@ -0,0 +1,13 @@
+#!/opt/isv/virtualenvs/lb_automation/bin/python
+
+class box:
+	def area(self):
+		return self.width * self.height
+
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+
+x = box(10, 2)
+print(x.area())
+
